a4ebef0c86d192a5d3f3737d6f07706f29926fc9
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2022-01-30 00:52:04 +0300

    Change Linkedin url

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,7 @@ export default function Introduction({name, role}) {
             <h3 style= {{fontSize:'24px'}}>{role}</h3>
             <SocialIcon url='mailto:onursimsek0643@gmail.com?Subject=About%20meeting' bgColor='white' style={{marginRight:'8px'}} />
             <SocialIcon url='https://github.com/simsekonur' bgColor='white' style={{marginRight:'8px'}} />
-            <SocialIcon url='www.linkedin.com/in/simsek-onur' bgColor='white' style={{marginRight:'8px'}} />
+            <SocialIcon url='https://www.linkedin.com/in/simsek-onur' bgColor='white' style={{marginRight:'8px'}} />
             <SocialIcon url='https://www.instagram.com/smsk_onur/' bgColor='white'/>
         </div>
     );
