68c0dca3c7f3d59cc22bbc3abaf5f1c2eb4d004b
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2023-03-07 01:23:21 +0300

    Update content

--- src/content.js
+++ src/content.js
@@ -1,12 +1,12 @@
 export const about = `My name is Onur ŞİMŞEK. I have graduated from the Computer Engineering Deparment of Middle East
-Technical University. I am from Ankara, Turkey and I am 24 years old. Currently, I am a Full Stack
-Software Developer at Avena Information Technologies.`
+Technical University. I am from Ankara, Turkey. I am helpful, dynamic and hardworking mid-level software engineer with
+ a strong background in computer science and professional working experience.`
 
 export const education = [`Middle East Technical University`, `Aydınlıkevler Anatolian High School`]
 
 export const interests= [`Desktop Application Development`, `Web Application Development`]
 
-export const pls= [`C/C++`,`C#`,`Java`,`JavaScript`]
+export const pls= [`C/C++`,`C#`,`Java`,`JavaScript`, `TypeScript`]
 
 export const frameworks= [`Qt`,`React`]
 
